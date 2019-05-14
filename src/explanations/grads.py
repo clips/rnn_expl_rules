fixed grad backprop in eval mode, saved fname, refactoring

src/explanations/grads.py
@@ -6,16 +6,16 @@ import torch
 import numpy as np
 
 class Explanation:
-    '''
+    """
     Base class for all explanations.
     Parameters: model, data, and explanation?
-    '''
+    """
     def __init__(self, method):
         self.method = method
         # save explanations in attribute imp_scores later
 
     @classmethod
-    def get_grad_importance(cls, model, corpus, corpus_encoder, grad_pooling, model_type):
+    def get_grad_importance(cls, model, corpus, corpus_encoder, grad_pooling, model_type, data_subset):
         '''
         Compute word importance scores based on backpropagated gradients
         :param model: model to compute importance scores for
@@ -24,6 +24,7 @@ class Explanation:
         :param grad_pooling: (dot|sum|max|l2|max_mul|l2_mul)
                               pooling technique for combining embedding dimension importance into word importance
         :param model_type: gru/lstm
+        :param data_subset: which data subset are the importance scores for
         '''
         grad_pooling = grad_pooling.lower()
 
@@ -32,17 +33,16 @@ class Explanation:
 
         inst = cls('grad_' + grad_pooling)
 
-        #activating setting to register hook. Needs to be done before the forward pass.
+        model.eval()
+        # IMP! backward doesnt work in eval mode unless we disable cudnn
+        torch.backends.cudnn.enabled = False  # disabling cudnn allows for backward in eval mode
+
+        # activating setting to register hook. Needs to be done before the forward pass.
         model.word_embeddings.requires_emb_grad = True
 
         global_imp_lst = list()
         gold_lst = list()
         pred_lst = list()
-
-        # model.eval() # IMP! backward doesnt work in eval mode - open issue.
-        # Setting model to train mode and then toggling parameters manually
-        model.train()
-        TorchUtils._set_eval(model)
 
         for idx, (cur_insts, cur_labels) in enumerate(corpus_encoder.get_batches_from_corpus(corpus, model.batch_size)):
             cur_insts, cur_labels, cur_lengths = corpus_encoder.batch_to_tensors(cur_insts, cur_labels, model.device)
@@ -51,7 +51,7 @@ class Explanation:
             fwd_out = model.forward(cur_insts, cur_lengths, model.hidden_in)
             preds = torch.argmax(fwd_out.detach(), 1)
 
-            #converting log softmax to softmax for gradient computation
+            # converting log softmax to softmax for gradient computation
             fwd_out = torch.exp(fwd_out)
 
             # sequence embeddings. Shape seq_len * batch_size * emb_dim
@@ -62,12 +62,13 @@ class Explanation:
             grad_tensor[np.arange(len(preds)), preds] = 1
 
             model.zero_grad()
+
             fwd_out.backward(grad_tensor)
 
             # Get the grads saved in the hook here, shape: seq_len * batch_size * emb_dim
             grads = model.word_embeddings.last_grad
 
-            model.detach_hidden_() #free up the computation graph
+            model.detach_hidden_() # free up the computation graph
 
             # word_imp shape: batch_size * seq_len
             word_imp = getattr(GradPooling, grad_pooling)(grads, embs)
@@ -86,18 +87,19 @@ class Explanation:
 
         inst.imp_scores = global_imp_lst
 
-        seq_lst = corpus_encoder.get_decoded_sequences(corpus, strip_angular = True)
+        seq_lst = corpus_encoder.get_decoded_sequences(corpus, strip_angular=True)
 
         inst.save(global_imp_lst, seq_lst, pred_lst, gold_lst,
                   fname = 'imp_scores_' +
                           model_type +
                           '_hid' + str(model.hidden_dim) +
                           '_emb' + str(model.emb_dim) +
+                          '_' + data_subset +
                           '_' + grad_pooling + '.json'
                  )
         return inst
 
-    def save(self, imp_scores, seqs, preds, golds, fname, dir_out = '../out/'):
+    def save(self, imp_scores, seqs, preds, golds, fname, dir_out='../out/'):
         # saving the sequences, the importance scores, and the gold and predicted labels as JSON file
         FileUtils.write_json(
             {'seq_lst': seqs,
@@ -112,7 +114,7 @@ class GradPooling:
     def dot(grads, embs):
         # Take dot product between grads and emb_weights to get overall word imp scores.
         # Would be the same as taking element-wise product and them summing across emb_dim
-        return torch.mul(embs, grads).sum(dim = 2).detach().transpose(0,1)
+        return torch.mul(embs, grads).sum(dim = 2).detach().transpose(0, 1)
 
     @staticmethod
     def sum(grads, embs = None):

src/torch_utils.py
@@ -45,13 +45,7 @@ class TorchUtils:
 
         return state
 
-    @staticmethod
-    def _set_eval(model):
-        '''
-        Switch off the training behaviour of the parameters
-        '''
-        for p in model.parameters():
-            p.train = False
+
 
 class EmbeddingMul(nn.Module):
     """This class implements a custom embedding module which registers a hook to save gradients.